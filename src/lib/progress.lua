53d60f4a6d05b2b0f8a52e890528d299ddca8454
LordBaryhobal <lordbaryhobal@gmail.com>
2025-11-29 14:45:41 +0100

    fix: correct progress bar width

--- src/lib/progress.lua
+++ src/lib/progress.lua
@@ -5,7 +5,9 @@ function progress.bar(x, y, width, value, max, fg, bg)
     local fgWidth = utils.round(value * width / max)
     fgWidth = math.max(0, math.min(width, fgWidth))
     paintutils.drawLine(x, y, x + width - 1, y, bg)
-    paintutils.drawLine(x, y, x + fgWidth - 1, y, fg)
+    if fgWidth > 0 then
+        paintutils.drawLine(x, y, x + fgWidth - 1, y, fg)
+    end
     term.setBackgroundColor(colors.black)
 end
 
