472634054f24edb7161ae94bc2869f7117eed196
LordBaryhobal <lordbaryhobal@gmail.com>
2025-12-07 13:44:31 +0100

    feat: solve day 7 puzzle 2

--- src/day07/puzzle2.lua
+++ src/day07/puzzle2.lua
@@ -1,7 +1,37 @@
+local utils = require "utils"
+
 local puzzle2 = {}
 
 function puzzle2.solve(input)
-    return 0
+    local lines = utils.splitLines(input)
+
+    local beams = {}
+
+    for i, line in ipairs(lines) do
+        local newBeams = {}
+        if i == 1 then
+            newBeams[line:find("S")] = 1
+        else
+            for j=1, #line do
+                if beams[j] then
+                    if line:sub(j, j) == "^" then
+                        newBeams[j - 1] = (newBeams[j - 1] or 0) + beams[j]
+                        newBeams[j + 1] = (newBeams[j + 1] or 0) + beams[j]
+                    else
+                        newBeams[j] = (newBeams[j] or 0) + beams[j]
+                    end
+                end
+            end
+        end
+        beams = newBeams
+    end
+
+    local finalBeams = 0
+    for _, v in pairs(beams) do
+        finalBeams = finalBeams + v
+    end
+
+    return finalBeams
 end
 
 return puzzle2
